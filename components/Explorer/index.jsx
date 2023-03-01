77666cdae154f959fc17896ea1f091ea47d51783
SreeXD <sree.nagaraj8@gmail.com>
2023-03-01 23:37:25 +0530

    quick fixes and refactoring

--- components/Explorer/index.jsx
+++ components/Explorer/index.jsx
@@ -1,5 +1,4 @@
 import { useAppContext } from "../../contexts/AppContext";
-import { useBundlrContext } from "../../contexts/BundlrContext";
 import Folder from "../Folder";
 import File from "../File";
 import style from "./style.module.css";
@@ -7,8 +6,6 @@ import DrivePreview from "../DrivePreview";
 
 export default function Explorer() {
   const { activateContextMenu, currentFile, currentFileData } = useAppContext();
-  const { fileSystem } = useBundlrContext();
-  const files = fileSystem.hierarchy.getFiles();
   const children = currentFileData?.children;
 
   if (currentFileData && currentFileData.type == "file") {
@@ -19,30 +16,6 @@ export default function Explorer() {
     );
   }
 
-  if (currentFile === "root")
-    return (
-      <div
-        className={style.explorer}
-        onContextMenu={(e) => {
-          e.preventDefault();
-          activateContextMenu(true, {
-            type: "explorer",
-          });
-        }}
-      >
-        <div className={style.section}>
-          <h1 className={style.sectionTitle}>Drives</h1>
-          <div className={style.drives}>
-            {files
-              .filter((x) => x.type == "drive")
-              .map((x) => (
-                <DrivePreview key={x.id} data={x} />
-              ))}
-          </div>
-        </div>
-      </div>
-    );
-
   return (
     <div>
       <div
@@ -50,31 +23,46 @@ export default function Explorer() {
         onContextMenu={(e) => {
           e.preventDefault();
           activateContextMenu(true, {
-            type: "drive",
+            type: currentFile == "root" ? "explorer" : "drive",
           });
         }}
       >
-        <div className={style.section}>
-          <h1 className={style.sectionTitle}>Folders</h1>
-          <div className={style.folders}>
-            {children
-              .filter((x) => x.type == "folder")
-              .map((x) => (
-                <Folder key={x.id} data={x} />
-              ))}
+        {
+          currentFile == 'root'
+          ? <div className={style.section}>
+            <h1 className={style.sectionTitle}>Drives</h1>
+            <div className={style.drives}>
+              {children && children
+                .map((x) => (
+                  <DrivePreview key={x.id} data={x} />
+                ))}
+            </div>
           </div>
-        </div>
 
-        <div className={style.section}>
-          <h1 className={style.sectionTitle}>Files</h1>
-          <div className={style.files}>
-            {children
-              .filter((x) => x.type == "file")
-              .map((x) => (
-                <File key={x.id} data={x} />
-              ))}
-          </div>
-        </div>
+          : <>
+            <div className={style.section}>
+              <h1 className={style.sectionTitle}>Folders</h1>
+              <div className={style.folders}>
+                {children && children
+                  .filter((x) => x.type == "folder")
+                  .map((x) => (
+                    <Folder key={x.id} data={x} />
+                  ))}
+              </div>
+            </div>
+
+            <div className={style.section}>
+              <h1 className={style.sectionTitle}>Files</h1>
+              <div className={style.files}>
+                {children && children
+                  .filter((x) => x.type == "file")
+                  .map((x) => (
+                    <File key={x.id} data={x} />
+                  ))}
+              </div>
+            </div>
+          </>
+        }
       </div>
     </div>
   );

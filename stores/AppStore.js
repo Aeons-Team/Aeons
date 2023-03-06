8b370467128a647a35921fd7146be429c6457d02
Jubin Jose <jubin2john@gmail.com>
2023-03-07 01:16:50 +0530

    added account login request

--- stores/AppStore.js
+++ stores/AppStore.js
@@ -3,23 +3,21 @@ import { shallow } from "zustand/shallow";
 import Vector2 from "../lib/Vector2";
 
 export const useAppStore = create((set, get) => ({
-    cursorPosition: new Vector2(),
-    contextMenuActivated: false,
-    contextMenuOpts: {},
-    contextMenuPosition: new Vector2(),
-    activateContextMenu: (flag, opts) => {
-        const { cursorPosition, contextMenuPosition } = get();
-        const partial = { contextMenuActivated: flag }
+  cursorPosition: new Vector2(),
+  contextMenuActivated: false,
+  contextMenuOpts: {},
+  contextMenuPosition: new Vector2(),
+  activateContextMenu: (flag, opts) => {
+    const { cursorPosition, contextMenuPosition } = get();
+    const partial = { contextMenuActivated: flag };
 
-        if (flag) {
-            contextMenuPosition.copy(cursorPosition)
-            partial.contextMenuOpts = opts
-        }
+    if (flag) {
+      contextMenuPosition.copy(cursorPosition);
+      partial.contextMenuOpts = opts;
+    } else partial.contextMenuOpts = {};
 
-        else partial.contextMenuOpts = {}
-
-        set(partial)
-    }
+    set(partial);
+  },
 }));
 
 export const useAppState = (func) => useAppStore(func, shallow);

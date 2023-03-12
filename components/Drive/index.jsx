6bfff8d6c24721e932e721ce14c9ec0afb86106c
SreeXD <sree.nagaraj8@gmail.com>
2023-03-12 18:39:28 +0530

    refactor code

--- components/Drive/index.jsx
+++ components/Drive/index.jsx
@@ -2,17 +2,27 @@ import Wallet from "../Wallet";
 import Explorer from "../Explorer";
 import ContextMenu from "../ContextMenu";
 import Hierarchy from "../Hierarchy";
-import Encrypt from "../Encrypt";
+import Button from "../Button"
+import { useBundlrStore } from "../../stores/BundlrStore";
 import style from "./style.module.css";
 
 export default function Drive() {
+  const fileSystem = useBundlrStore(state => state.fileSystem)
+
   return (
     <div className={style.drive}>
       <ContextMenu />
 
       <div className={style.driveGrid}>
         <Wallet />
-        <Encrypt />
+        <Button
+          onClick={async () => {
+            const tx = await fileSystem.createUser()
+            console.log(tx)
+          }}
+        >
+          Create User
+      </Button>
         <div />
         <Hierarchy />
         <Explorer />

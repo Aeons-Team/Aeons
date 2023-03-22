4ad326d6b7f41a91716813b9a0eac3cd5f5f1a52
Jubin Jose <jubin2john@gmail.com>
2023-03-23 04:05:57 +0530

    add refactor search with async select

--- components/Search/index.jsx
+++ components/Search/index.jsx
@@ -1,39 +1,48 @@
-import { useState } from "react";
 import { useRouter } from "next/router";
 import { useBundlrState } from "../../stores/BundlrStore";
 import style from "./style.module.css";
+import AsyncSelect from "react-select/async";
 
 export default function Search() {
-  const [searchItem, setSearchItem] = useState("");
   const router = useRouter();
   const [fileSystem] = useBundlrState((state) => [state.fileSystem]);
   const files = fileSystem.hierarchy.tree.nodes;
   const filesList = {};
-  const searchList = [];
 
-  function SearchQuery() {
-    if (searchItem.trim() == "") return;
+  const filterOptions = (inputValue) => {
+    let searchList = [];
 
-    Object.values(files).map((file) => (filesList[file.id] = file.name));
-    for (const key in filesList) {
-      filesList[key].toLowerCase().includes(searchItem.trim().toLowerCase()) &&
-        searchList.push(key);
+    Object.values(files).map((file) => {
+      file.id !== "root" && (filesList[file.id] = file.name);
+    });
+
+    for (const [id, name] of Object.entries(filesList)) {
+      name.toLowerCase().includes(inputValue.trim().toLowerCase()) &&
+        searchList.push({ key: name, value: id, label: name });
     }
-    setSearchItem("");
-    router.push(
-      `/drive/search/${searchList.length ? searchList : "NoResults"}`
-    );
-  }
+    return searchList;
+  };
+
+  const loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+      callback(filterOptions(inputValue));
+    }, 1000);
+  };
 
   return (
     <div className={style.search}>
-      <input
-        type="text"
-        value={searchItem}
+      <AsyncSelect
+        cacheOptions
+        loadOptions={loadOptions}
+        noOptionsMessage={() => null}
         placeholder="Search"
-        onInput={(e) => setSearchItem(e.target.value)}
-        onKeyUp={(e) => {
-          e.key === "Enter" && SearchQuery();
+        blurInputOnSelect={true}
+        onChange={(e) => {
+          router.push(`/drive/search/${e.value}`);
+        }}
+        components={{
+          DropdownIndicator: () => null,
+          IndicatorSeparator: () => null,
         }}
       />
     </div>

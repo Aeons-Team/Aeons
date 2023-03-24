ca80a11c764bab91323aadd73f643e52e7fda194
SreeXD <sree.nagaraj8@gmail.com>
2023-03-25 04:30:47 +0530

    git add for the previous commit :v

---
+++ contracts/scripts/deployment/deploy.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+import fs from 'fs'
+import { Warp } from 'warp-contracts'
+import { DeployPlugin } from 'warp-contracts-plugin-deploy'
+
+const walletJwkPath = path.join(__dirname, '../../data/wallet.json')
+const initialStatePath = path.join(__dirname, '../../data/initialState.json')
+const contractSrcPath = path.join(__dirname, '../../pkg/xdrive_contract_bg.wasm')
+const wasmSrcCodeDirPath = path.join(__dirname, '../../src')
+const wasmGlueCodePath = path.join(__dirname, '../../pkg/xdrive_contract.js')
+const contractDeploymentPath = path.join(__dirname, '../../data/contract_deployment.json')
+
+async function deploy(warp: Warp) {
+    warp.use(new DeployPlugin())
+
+    const walletJwk = JSON.parse(fs.readFileSync(walletJwkPath, 'utf8'))
+    const initialStateJson = fs.readFileSync(initialStatePath, 'utf8')
+    const contractSrc = fs.readFileSync(contractSrcPath)
+
+    const contractDeploy = await warp.deploy({
+        wallet: walletJwk,
+        initState: initialStateJson,
+        src: contractSrc,
+        wasmSrcCodeDir: wasmSrcCodeDirPath,
+        wasmGlueCode: wasmGlueCodePath
+    })
+
+    console.log(`contract deployed successfully`)
+    console.log(`contract id: ${contractDeploy.contractTxId}`)
+    console.log(`contract source id: ${contractDeploy.srcTxId}`)
+
+    fs.writeFileSync(contractDeploymentPath, JSON.stringify(contractDeploy))
+}
+
+export default deploy

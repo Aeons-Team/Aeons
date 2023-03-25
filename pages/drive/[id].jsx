8e388a64138511dbdf1cd4005a8200b5030faa4f
SreeXD <sree.nagaraj8@gmail.com>
2023-03-26 04:00:30 +0530

    fix issue with clear selection

--- pages/drive/[id].jsx
+++ pages/drive/[id].jsx
@@ -1,11 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useDriveStore } from "../../stores/DriveStore";
 import { useAppState } from "../../stores/AppStore";
 import Drive from "../../components/Drive";
 
 function DrivePage() {
+  const { id } = useRouter().query
   const initialized = useDriveStore((state) => state.initialized);
   const [ clearSelection ] = useAppState((state) => [state.clearSelection]);
-  clearSelection();
+
+  useEffect(() => {
+    clearSelection();
+  }, [id])
 
   if (initialized) {
     return <Drive />;

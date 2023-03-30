f264ac6e3b297a6e8386db922927c3a654b0169b
4k11 <akil.dhnx@gmail.com>
2023-03-30 11:42:55 +0530

    refined theme factors

--- components/ModeButton/index.jsx
+++ components/ModeButton/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useEffect } from "react";
 import Button from "../Button";
 import style from "./style.module.css";
 
@@ -7,17 +6,22 @@ export default function ModeButton() {
 
   const [mode, setMode] = useState("dark");
   const toggleMode = () => {
+
+    var r = document.querySelector(':root');
+
     if(mode === "dark"){
+      r.style.setProperty('--color-primary', '#FFFFFF');
+      r.style.setProperty('--color-secondary', '#000000');
+      r.style.setProperty('--color-background', '#F8F8F8');
       setMode("light");
     }
     else {
+      r.style.setProperty('--color-primary', '#000000');
+      r.style.setProperty('--color-secondary', '#FFFFFF');
+      r.style.setProperty('--color-background', '#0c0c0c');
       setMode("dark"); 
     }
   };
-
-  useEffect(() => {
-    document.body.className = mode;
-  }, [mode]);
   
   return (
     <div className={style.modeButton}>
@@ -26,7 +30,7 @@ export default function ModeButton() {
           toggleMode()
         }}
       >
-        Mode
+        Theme
       </Button>
     </div>
   );
